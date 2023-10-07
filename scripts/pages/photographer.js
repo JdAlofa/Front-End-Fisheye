1efdaf3796abe8fcdc5d592bf96084c806713e6f
JdAlofa <alofa.jeandavid@gmail.com>
2023-10-07 14:36:09 +0200

    Before filtering arrays of data

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,11 @@
-//Mettre le code JavaScript lié à la page photographer.html
+function photographerPage(data) {
+    
+
+
+}
+
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("id");
+console.log(id); // Display the id parameter in the console
+
+link.setAttribute("href", `./photographer.html?id=${id}`);
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,39 @@
-function photographerTemplate(data) {
-    const { name, portrait } = data;
+export function photographerTemplate(data) {
+  const { name, portrait, city, country, tagline, price,id } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `../../FishEye_Photos/Sample Photos/Photographers ID Photos/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+    img.setAttribute("alt", name);
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+
+    const location = document.createElement("span");
+    location.classList.add("location");
+    location.textContent = `${city}, ${country}`;
+
+    const tagliney = document.createElement("span");
+    tagliney.classList.add("tagline");
+    tagliney.textContent = `${tagline}`;
+
+    const pricey = document.createElement("span");
+    pricey.classList.add("price");
+    pricey.textContent = `${price}€/jour`;
+
+    const link = document.createElement("a");
+    link.setAttribute("href", `photographer.html?id=${id}name=${name}`);
+
+    article.appendChild(link);
+    link.appendChild(img);
+    link.appendChild(h2);
+    article.appendChild(location);
+    article.appendChild(tagliney);
+    article.appendChild(pricey);
+
+    return article;
+  }
+  return {  getUserCardDOM };
 }
